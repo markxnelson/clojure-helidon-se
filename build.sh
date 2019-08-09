ed47bc7b4861f2d2557c32116ef5fd43ce5b1e62
Mark Nelson <mark.x.nelson@oracle.com>
2019-08-09 16:37:39 -0400

    working on native-image build

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+GRAALVM_HOME=/home/mark/graalvm-ee-19.1.1
+JAVA_HOME=/home/mark/graalvm-ee-19.1.1
+PATH=$JAVA_HOME/bin:$PATH
+
+function status () {
+  echo -e "\e[1m$1\e[0m"
+}
+
+status 'Build the uberjar...'
+lein uberjar
+
+status 'Run the application with tracing agent...'
+rm -rf config-dir trace-file.json
+java \
+  -agentlib:native-image-agent=trace-output=$PWD/trace-file.json \
+  -jar target/uberjar/clojure-se-0.1.0-SNAPSHOT-standalone.jar &
+PID=$!
+
+status 'Make a few client requests...'
+sleep 4
+curl http://localhost:8080/bob
+curl http://localhost:8080/dave
+sleep 4
+
+status 'Kill the application...'
+kill -9 $PID
+
+status 'Create native image configuration files...'
+native-image-configure \
+  generate \
+  --trace-input=$PWD/trace-file.json \
+  --output-dir=$PWD/config-dir
+
+status 'Build native image...'
+native-image \
+  --allow-incomplete-classpath \
+  --no-fallback \
+  -H:ConfigurationFileDirectories=$PWD/config-dir \
+  -jar target/uberjar/clojure-se-0.1.0-SNAPSHOT-standalone.jar 
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-se "0.1.0-SNAPSHOT"
   :description "A clojure Helidon SE app"
   :dependencies [[org.clojure/clojure "1.10.0"]
-               [io.helidon/helidon-bom "1.1.2" :extension "pom"]
+               [io.helidon/helidon-bom "1.2.0" :extension "pom"]
                [io.helidon.bundles/helidon-bundles-webserver "1.1.2"]
                [io.helidon.config/helidon-config-yaml "1.1.2"]
                [io.helidon.health/helidon-health "1.1.2"]
